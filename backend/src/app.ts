2b77c4c4096a226ade60ed09db014441abf71d1f
Supaishichikin <jordan.toko@outlook.fr>
2023-05-15 00:26:23 +0200

    register route
    register route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,17 +5,44 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Connexion à la base de données
-import { connectDB } from "./db/conn";
-
+import { connectDB, User } from "./db/conn";
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const port = 8030;
 const app = express();
 const corsOptions = { origin: "http://localhost:3000" };
 app.use(cors(corsOptions));
-
+app.use(express.json());
 connectDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello World!" });
+});
+
+app.post('/register', async (req, res) => {
+  const { username, password, first_name, last_name, phone, email } = req.body;
+
+  try {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user document in MongoDB
+    const newUser = new User({
+      username: username,
+      password: hashedPassword,
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      phone: phone
+    });
+
+    await newUser.save();
+
+    res.json({ message: 'Registration successful' });
+  } catch (error) {
+    console.error('Error registering user:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
 app.listen(port, () => {
--- backend/src/db/conn.ts
+++ backend/src/db/conn.ts
@@ -1,9 +1,24 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  phone: String,
+  email: String,
+  password: String,
+  role: {
+    type: String,
+    enum: ['USER', 'ADMIN'],
+    default: 'USER',
+  },
+});
+
+export const User = mongoose.model('User', userSchema);
 
 export const connectDB = async () => {
   try {
     mongoose.set("strictQuery", true);
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, dbName: 'sports_products' });
 
     console.log("MongoDB à l'écoute");
   } catch (err) {
