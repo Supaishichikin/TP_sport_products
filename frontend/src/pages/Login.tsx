2a2e29a41c193c458b46bbb43f9864ff9c0e8768
Supaishichikin <jordan.toko@outlook.fr>
2023-05-15 09:56:08 +0200

    register form and page
    register form and page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -4,7 +4,7 @@ export default function Login(){
     return <>
     <div style={{height: "100%", width: "100%", alignItems: "center"}} className="relative flex flex-col justify-center overflow-hidden">
             <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-                <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
+                <h1 className="text-3xl font-semibold text-center text-purple-700">
                    Login
                 </h1>
                 <FormLogin/>
@@ -12,7 +12,7 @@ export default function Login(){
                     {" "}
                     Don't have an account?{" "}
                     <a
-                        href="#"
+                        href="/register"
                         className="font-medium text-purple-600 hover:underline"
                     >
                         Register
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,3 +1,24 @@
+import FormRegister from "../components/FormRegister";
+
 export default function Register(){
-    return <h1>Register</h1>
+    return <>
+    <div style={{height: "100%", width: "100%", alignItems: "center"}} className="relative flex flex-col justify-center overflow-hidden">
+            <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+                <h1 className="text-3xl font-semibold text-center text-purple-700">
+                    Register
+                </h1>
+                <FormRegister/>
+                <p className="text-xs font-light text-center text-gray-700">
+                    {" "}
+                    Already have an account?{" "}
+                    <a
+                        href="/login"
+                        className="font-medium text-purple-600 hover:underline"
+                    >
+                        Log in
+                    </a>
+                </p>
+            </div>
+        </div>
+    </>
 }
