c408b589456b7b97423543ff4292993ce33074a1
bflorestal <yayou7710@hotmail.fr>
2023-05-15 15:23:14 +0200

    Page AdminProducts et ses components

---
+++ frontend/src/components/ProductsTable.tsx
@@ -0,0 +1,240 @@
+import type { Product } from "../types/product";
+import { deleteProduct, updateProduct } from "../api/products";
+
+export default function productsTable({ products }: { products: Product[] }) {
+  return (
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table className="w-full text-sm text-left text-gray-500">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+          <tr>
+            {["Product name", "Price", "Sport", "Action 1", "Action 2"].map(
+              (column, index) => (
+                <th key={index} scope="col" className="px-6 py-3">
+                  {column}
+                </th>
+              )
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <ProductRow key={product._id} {...product} />
+          ))}
+        </tbody>
+      </table>
+      <div
+        id="editproductModal"
+        tabIndex={-1}
+        aria-hidden="true"
+        className="fixed top-0 left-0 right-0 z-50 items-center justify-center hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      >
+        <div className="relative w-full max-w-2xl max-h-full">
+          {/* Modal content */}
+          <form action="#" className="relative bg-white rounded-lg shadow">
+            {/* Modal header */}
+            <div className="flex items-start justify-between p-4 border-b rounded-t">
+              <h3 className="text-xl font-semibold text-gray-900">
+                Edit product
+              </h3>
+              <button
+                type="button"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
+                data-modal-hide="editproductModal"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+            {/* Modal body */}
+            <div className="p-6 space-y-6">
+              <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="last-name"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                    placeholder="Green"
+                    required
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                    placeholder="example@company.com"
+                    required
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="phone-number"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    Phone Number
+                  </label>
+                  <input
+                    type="number"
+                    name="phone-number"
+                    id="phone-number"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                    placeholder="e.g. +(12)3456 789"
+                    required
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="department"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Department
+                  </label>
+                  <input
+                    type="text"
+                    name="department"
+                    id="department"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                    placeholder="Development"
+                    required
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="company"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Company
+                  </label>
+                  <input
+                    type="number"
+                    name="company"
+                    id="company"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                    placeholder="123456"
+                    required
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="current-password"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Current Password
+                  </label>
+                  <input
+                    type="password"
+                    name="current-password"
+                    id="current-password"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                    placeholder="••••••••"
+                    required
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="new-password"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    New Password
+                  </label>
+                  <input
+                    type="password"
+                    name="new-password"
+                    id="new-password"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                    placeholder="••••••••"
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            {/* Modal footer */}
+            <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b">
+              <button
+                type="submit"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Save all
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ProductRow({ _id, name, price, quantity, sport }: Product) {
+  async function handleEdit() {
+    alert("Not implemented yet");
+  }
+
+  async function handleDelete() {
+    if (!window.confirm("Are you sure you want to delete this product?"))
+      return;
+    const data = await deleteProduct(_id);
+    console.log(data);
+
+    // TODO: Mettre à jour la liste des produits
+    window.location.reload();
+  }
+
+  return (
+    <tr className="bg-white border-b hover:bg-gray-50">
+      <th
+        scope="row"
+        className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap"
+      >
+        <div className="space-x-1">
+          <div className="inline text-base font-semibold">{name}</div>
+          <div className="inline font-normal text-gray-500">x{quantity}</div>
+        </div>
+      </th>
+      <td className="px-6 py-4">{price} €</td>
+      <td className="px-6 py-4">{sport}</td>
+      <td className="px-6 py-4">
+        <button
+          type="button"
+          onClick={handleEdit}
+          className="text-sm font-medium text-blue-600 hover:underline"
+        >
+          Edit product
+        </button>
+      </td>
+      <td className="px-6 py-4">
+        <button
+          type="button"
+          onClick={handleDelete}
+          className="text-sm font-medium text-red-600 hover:underline"
+        >
+          Delete product
+        </button>
+      </td>
+    </tr>
+  );
+}
